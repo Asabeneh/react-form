31ca4685d012dbd4c9ad14d36d28cfee3c5e8581
Asabeneh <asabeneh@gmail.com>
2020-03-08 19:22:40 +0200

    Updates

---
+++ precache-manifest.17538aefde70f83f67a3a0588d050fe6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd8cc51e0cca118decbf1a20eea3ff4c",
+    "url": "/react-form/index.html"
+  },
+  {
+    "revision": "d172fd0895e94798bd0e",
+    "url": "/react-form/static/css/main.7e1e1f6f.chunk.css"
+  },
+  {
+    "revision": "ca288a11731f72c40faf",
+    "url": "/react-form/static/js/2.dbe85b39.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-form/static/js/2.dbe85b39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d172fd0895e94798bd0e",
+    "url": "/react-form/static/js/main.ee7c7073.chunk.js"
+  },
+  {
+    "revision": "a7d2e95bc7d7bc445b48",
+    "url": "/react-form/static/js/runtime-main.5702d1ca.js"
+  }
+]);
